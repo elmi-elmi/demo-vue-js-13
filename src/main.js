74d616442d7f94aee6d905fc743a1edbf373092d
elmi <shahrokhelmi@gmail.com>
2021-09-15 18:43:20 +0430

    from the start of video 5

--- src/main.js
+++ src/main.js
@@ -2,10 +2,20 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router'
 
 import App from './App.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamsList from './components/teams/TeamsList.vue';
 const router = createRouter({
     history: createWebHistory(),
-    routes: []
+    routes: [{
+        path: '/user',
+        component: UsersList
+    },
+    {
+        path: '/team',
+        component: TeamsList
+    }]
 });
 const app = createApp(App)
+app.use(router)
 
 app.mount('#app');
